f70b9aa07966694d41a186e24cddd7b3dd18cad3
anangzuhadiismail-alt <77595169+anangzuhadiismail-alt@users.noreply.github.com>
2022-05-24 17:29:17 +0700

    Operasi Aritmatika

---
+++ Main.java
@@ -0,0 +1,30 @@
+package operatorAritmatika;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("===== operasi penjumlahan =====");
+         int angkaPenjumlahan = 7 + 8;
+        System.out.println(" hasil dari 7 + 8  = " + angkaPenjumlahan);
+        System.out.println();
+
+        System.out.println("===== operasi pengurangan =====");
+        int angkaPengurangan = 7 - 8;
+        System.out.println(" hasil dari 7 - 8  : " + angkaPengurangan);
+        System.out.println();
+
+        System.out.println("===== operasi perkalian =====");
+        int angkaPerkalian = 7 * 8;
+        System.out.println(" hasil dari 7 * 8  : " + angkaPerkalian);
+
+        System.out.println("===== operasi pembagian =====");
+        int angkaPembagian = 7 / 8;
+        System.out.println(" hasil dari 7 / 8  : " + angkaPembagian);
+
+        System.out.println("===== operasi sisa pembagian =====");
+        int angkaSisaPembagian = 7 % 8;
+        System.out.println(" hasil dari pembagian 7 dan 8  : " + angkaSisaPembagian);
+
+    }
+}
