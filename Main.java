49bb66876463eff7b973f1809921caaa98fd8a08
anangzuhadiismail-alt <77595169+anangzuhadiismail-alt@users.noreply.github.com>
2022-05-24 17:31:41 +0700

    Assaignment Operator

--- Main.java
+++ Main.java
@@ -1,30 +1,14 @@
-package operatorAritmatika;
-
+package com.operationAssaignment;
+// operator assaignment Assignment Variable (penempatan/pengalamatan variabel) merupakan operator yang cukup simpel dan diwakilkan dengan simbol operator ‘=’.
+// Operator assigment memberikan nilai pada sebuah variabel yang ada di sebelah kiri (left operand) dengan nilai yang ada di sebelah kanan (right operand).
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("===== operasi penjumlahan =====");
-         int angkaPenjumlahan = 7 + 8;
-        System.out.println(" hasil dari 7 + 8  = " + angkaPenjumlahan);
-        System.out.println();
+        int value = 5;
+        int anotherValue = 30;
 
-        System.out.println("===== operasi pengurangan =====");
-        int angkaPengurangan = 7 - 8;
-        System.out.println(" hasil dari 7 - 8  : " + angkaPengurangan);
-        System.out.println();
-
-        System.out.println("===== operasi perkalian =====");
-        int angkaPerkalian = 7 * 8;
-        System.out.println(" hasil dari 7 * 8  : " + angkaPerkalian);
-
-        System.out.println("===== operasi pembagian =====");
-        int angkaPembagian = 7 / 8;
-        System.out.println(" hasil dari 7 / 8  : " + angkaPembagian);
-
-        System.out.println("===== operasi sisa pembagian =====");
-        int angkaSisaPembagian = 7 % 8;
-        System.out.println(" hasil dari pembagian 7 dan 8  : " + angkaSisaPembagian);
-
+        System.out.println(" data pada value >>" + value);
+        System.out.println(" data pada anotherValue>> " + anotherValue);
     }
 }
